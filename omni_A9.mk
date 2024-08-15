7462ed555684478f834e8c4ae9e18154a756376e
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-08-15 15:06:00 +0800

    Update omni_A9.mk

--- omni_A9.mk
+++ omni_A9.mk
@@ -9,8 +9,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from A9 device
 $(call inherit-product, device/alps/A9/device.mk)
